8f668be560cf093e1bdea2aad075f43d4e4b5a13
mo00ot <mootobject@duck.com>
2025-09-24 23:48:33 +0200

    Updated: added menu
    - added menu with search(1) and install(2), at least, exit(3)
    
    - removed .exe version
         defender revelate it as a trojan
    
    - only italian for the moment

--- Winget Searcher and Installer.bat
+++ Winget Searcher and Installer.bat
@@ -1,27 +1,42 @@
 @echo off
 Title Winget Searcher and Installer by Mo00ot.
 
-:start
-echo Search and install program with winget by mo00ot.
+:mainmenu
+cls
+echo ==========================================
+echo      Winget Searcher and Installer
+echo ==========================================
+echo.
+echo  1. Cerca un programma
+echo  2. Installa un programma
+echo  3. Esci
+echo.
+set /p choice="Seleziona un'opzione [1-3]: "
+
+if "%choice%"=="1" goto Search
+if "%choice%"=="2" goto Install
+if "%choice%"=="3" goto Exit
+goto mainmenu
 
 :Search
-echo Name of the program ? or type ext:
-SET /P name=""
-if %name% == ext goto exit
-winget search %name%
-
-:install
-echo Which program do you want to install ? or type ext:
-SET /p eman=""
-if %eman% == ext GOTO exit
-winget install %eman%
+cls
+echo === Ricerca Programma ===
+echo Inserisci il nome del programma da cercare (o 'menu' per tornare):
+set /p name="Nome: "
+if /i "%name%"=="menu" goto mainmenu
+winget search "%name%"
 pause
-echo Need anything else?
-echo Type y or n 
-set /p y=""
-if %y% == y cls goto Search (
-	if %y% == n GOTO exit )
-goto Search 
+goto mainmenu
 
-:exit
+:Install
+cls
+echo === Installa Programma ===
+echo Inserisci il nome del programma da installare (o 'menu' per tornare):
+set /p eman="Nome: "
+if /i "%eman%"=="menu" goto mainmenu
+winget install "%eman%"
+pause
+goto mainmenu
+
+:Exit
 exit
